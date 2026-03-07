67759f268f6f772ec7123006929c4d74254855a7
i-am-abs <abs.createx@gmail.com>
2026-03-07 23:36:18 +0530

    nearby masjid

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from contextlib import asynccontextmanager
 from fastapi import FastAPI, HTTPException
 from fastapi.middleware.cors import CORSMiddleware
-import uvicorn
+# import uvicorn
 
 from logger.Logger import Logger
 from api.routes import router
