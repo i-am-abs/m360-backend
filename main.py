07f771c5526bf59f76cc877c9b111eaa150f6730
i-am-abs <abs.createx@gmail.com>
2026-05-07 00:56:36 +0530

    refactored mongoDB codebase

--- main.py
+++ main.py
@@ -1,17 +1,20 @@
 from app.factory import create_app
+from dotenv import load_dotenv
 
 app = create_app()
 
-# if __name__ == "__main__":
-#     import uvicorn
-#
-#     from app.core.config import get_settings
-#
-#     settings = get_settings()
-#     uvicorn.run(
-#         "main:app",
-#         host="0.0.0.0",
-#         port=settings.server_port,
-#         reload=True,
-#         log_level=settings.logging_level.lower(),
-#     )
+load_dotenv("/home/arpit_createx/m360-backend/.env")
+
+if __name__ == "__main__":
+    import uvicorn
+
+    from app.core.config import get_settings
+
+    settings = get_settings()
+    uvicorn.run(
+        "main:app",
+        host="0.0.0.0",
+        port=settings.server_port,
+        reload=True,
+        log_level=settings.logging_level.lower(),
+    )
